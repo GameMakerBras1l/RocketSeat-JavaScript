5311ace687e2435b78af52eb0d04d2a8a4d5c781
GameMakerBras1l <marcelciro2015@gmail.com>
2026-04-10 15:35:09 -0300

    UPGRADE

--- scripts.js
+++ scripts.js
@@ -8,6 +8,8 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 // Manipulando o input amount para receber apenas numeros
 amount.addEventListener("input", () => {
@@ -35,8 +37,26 @@ form.onsubmit = (event) => {
 // Função para converter o valor para a moeda selecionada
 function convertCurrency(amount, price, symbol) {
     try {
+        // Exibindo a cotação da moeda selecionada
+        description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+        
+        // Calcula o total
+        let total = amount * price
+
+        // Formatar o valor total
+        total = formatCurrencyBRL(total).replace("R$", "")
+
+        // Verifica se o resultado é um numero
+        if(isNaN(total)){
+            return alert("Por favor digite o valor corretamente, para ser convertido.")
+        }
+
+        // Exibir o resultado total
+        result.textContent = `${total} Reais`
+
         // Aplica a classe que exibe o footer para mostrar o resultado
         footer.classList.add("show-result")
+
     } catch (error) {
         console.log(error)
         
@@ -44,4 +64,13 @@ function convertCurrency(amount, price, symbol) {
         footer.classList.remove("show-result")
         alert("Ocorreu um erro ao converter a moeda. Por favor, tente novamente.")
     }
+}
+
+// Formata a moeda para o formato brasileiro
+function formatCurrencyBRL(value) {
+    // Converte para número para utilizar o método toLocaleString e formata para o estilo de moeda brasileira
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
 }
